e4dfd58485769bd96536d5c95277bb7d701f7f83
himawaria <yui.okabe0406@gmail.com>
2023-12-16 23:19:31 +0900

    Committed1

---
+++ src/test/java/com/kadai/thymeleaf/KadaiThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadai.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KadaiThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
